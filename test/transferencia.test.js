a4275a10dd0d7a4bfa7c33f5d62b0d0dbecb27d7
Laiane Cunha <laianecunha20@gmail.com>
2025-10-28 10:17:44 -0300

    Adicionando o hook de beforeEach para captura do token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,9 +5,13 @@ const {obterToken} = require('../helpers/autenticacao')
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
-        it('Deve retornar sucesso com 201 quando o valor da transferência for acima de R$ 10,00 reais', async () => {
-            const token = await obterToken('julio.lima', '123456')
-            
+        let token;
+
+        beforeEach(async() => {
+            token = await obterToken('julio.lima', '123456')
+        })
+
+        it('Deve retornar sucesso com 201 quando o valor da transferência for acima de R$ 10,00 reais', async () => {                    
             const resposta = await request (process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
@@ -18,15 +22,13 @@ describe('Transferencias', () => {
                             valor: 11,
                             token: ""
                     })
-                    expect(resposta.status).to.equal(201);
 
+                    expect(resposta.status).to.equal(201);
                   //  console.log(resposta.body)
         })
     
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00 reais', async () => {
-            const token = await obterToken('julio.lima', '123456')
-
             const resposta = await request ('http://localhost:3000')
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
@@ -37,8 +39,8 @@ describe('Transferencias', () => {
                             valor: 7,
                             token: ""
                     })
-                    expect(resposta.status).to.equal(422);
 
+                    expect(resposta.status).to.equal(422);
         })
     })
 })
